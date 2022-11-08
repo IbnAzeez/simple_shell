184cd78fafb807d30cc779929e95057470fdcaf6
IbnAzeez <hayzed003@gmail.com>
2022-11-08 03:53:04 -0800

    Shell

--- shell.h
+++ shell.h
@@ -19,5 +19,11 @@ char *_strdup(char *str);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 void cannot_find(char *commandd);
+int portable_command(char *command_array[]);
+char *_getenv(char *variable);
+int _strn_cmp(char *s1, char *s2, int n);
+int command_line(char *s, size_t __attribute__((unused))file_stream);
+char *find(char *command);
+int _command(char *command_array[]);
 
 #endif
