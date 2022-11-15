e1671d6061f8829d9626bc1f7fd46f71c0627a9b
IbnAzeez <hayzed003@gmail.com>
2022-11-15 00:58:34 -0800

    Fixing errors

--- _strn_cmp.c
+++ _strn_cmp.c
@@ -1,22 +1,21 @@
 #include "shell.h"
 
 /**
- * _strn_cmp - compares n chars from two strings
- * @s1: first string
- * @s2: second string to compare to first string
- * @n: no of the comparison between s1 and s2
- * Return: <0 if s1 is less than s2, 0 for equal, >0 if s1 is greater than s2
+ * _strn_cmp - compares two strings up to n length
+ * @s: string to search for
+ * @var: variable to compare against
+ * @n: length to compare up to
+ * Return: 1 if strings are equal, -1 if they are not
  */
-int _strn_cmp(char *s1, char *s2, int n)
+int _strn_cmp(char *s, char *var, unsigned int n)
 {
-	char c1, c2;
+	unsigned int i = 0;
 
-	while (n--)
+	while (i < length)
 	{
-		c1 = *s1++;
-		c2 = *s2++;
-		if (c1 == '\0' || c1 != c2)
-			return (c1 > c2 ? 1 : (c1 < c2 ? -1 : 0));
+		if (s[i] != var[i])
+			return (-1);
+		i++;
 	}
-	return (0);
+	return (1);
 }
