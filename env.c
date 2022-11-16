efa6c415748e1f43c060e7153bd35067ab1d7264
maranathaadesanya <adesanyamaranatha@yahoo.com>
2022-11-16 15:33:38 -0800

    Hot fix

--- env.c
+++ env.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * _myenv - prints current environment
- * @info: structure containing potential arguments used to maintain
- * constant function prototype
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
  * Return: Always 0
  */
 
@@ -14,10 +14,11 @@ int _myenv(info_t *info)
 }
 
 /**
- * _getenv - gets value of environ variable
- * @info: structure containing potential arguments
+ * _getenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
  * @name: env var name
- * Return: value
+ *
+ * Return: the value
  */
 
 char *_getenv(info_t *info, const char *name)
@@ -36,9 +37,10 @@ char *_getenv(info_t *info, const char *name)
 }
 
 /**
- * _mysetenv - Initialize new environment variable or modify
- * @info: structure containing potential arguments used to maintain
- * constant function prototype
+ * _mysetenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
  *  Return: Always 0
  */
 
@@ -51,13 +53,14 @@ int _mysetenv(info_t *info)
 	}
 	if (_setenv(info, info->argv[1], info->argv[2]))
 		return (0);
+
 	return (1);
 }
 
 /**
- * _myunsetenv - remove environment variable
- * @info: structure containing potential arguments used to maintain
- * constant function prototype.
+ * _myunsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
  *  Return: Always 0
  */
 
@@ -78,8 +81,8 @@ int _myunsetenv(info_t *info)
 
 /**
  * populate_env_list - populates env linked list
- * @info: structure containing potential arguments used to maintain
- * constant function prototype
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
  * Return: Always 0
  */
 
